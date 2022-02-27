b11043ff895d7b7b4221ad5d0e732e6304baa005
Harsh Raj <harshraj9812@gmail.com>
2022-02-27 11:38:44 +0530

    Changed Server Port for Heroku.

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ const server = express()
 server.all("/", (req, res) => {
   res.send("Bot is Running !")
 })
-
+const port = process.env.PORT || 3000;
 function keepAlive() {
-  server.listen(3000, () => {
+  server.listen(port, () => {
     console.log("Server is Ready .")
   })
 }
