8b8edbe9a73331fcef656971462ad5fbf1bccf6a
Harshraj9812 <harshraj9812@gmail.com>
2022-06-17 13:41:36 +0530

    Rolled back to deploy with own server removed Vercel Config.

--- server.js
+++ server.js
@@ -1,21 +1,15 @@
-const express = require("express");
+const express = require("express")
 
 const server = express()
 
-server.get("/", (req, res) => {
-  res.send("Bot is Running !");
-});
+server.all("/", (req, res) => {
+  res.send("Bot is Running !")
+})
+const port = process.env.PORT || 3000;
+function keepAlive() {
+  server.listen(port, () => {
+    console.log("Server is Ready .")
+  })
+}
 
-// Added for Vercel Deployment for Serverless functions
-server.listen(5000, () => {
-  console.log("Running on Port 5000.");
-});
-
-// const port = process.env.PORT || 3000;
-// function keepAlive() {
-//   server.listen(port, () => {
-//     console.log("Server is Ready .")
-//   })
-// }
-
-module.exports = keepAlive;
+module.exports = keepAlive
